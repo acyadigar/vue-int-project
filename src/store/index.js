9e266bcc0aa2583496f88e0213147e3ac1f085ae
acyadigar <alpercanyadigar@gmail.com>
2021-12-08 17:57:40 +0300

    Add fetching actions to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
+
+const http = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+});
 
 export default createStore({
   state: {
+    users: [],
   },
   mutations: {
+    SET_USERS(state, data) {
+      state.users = data;
+    },
   },
   actions: {
+    async fetchUsers({ commit }) {
+      const result = await http.get("/users");
+      commit("SET_USERS", result.data);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
