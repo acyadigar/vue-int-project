0ef71828595b36693a5be5a29d6420fe978f05da
acyadigar <alpercanyadigar@gmail.com>
2021-12-09 00:30:44 +0300

    Upload home and styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SideBar from "@/components/SideBar.vue";
+import Users from "@/components/Users.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    SideBar,
+    Users
+  },
+};
 </script>
+
+
+<template>
+  <div class="home">
+    <SideBar />
+    <Users currentRoute='Anasayfa'/>
+  </div>
+</template>
