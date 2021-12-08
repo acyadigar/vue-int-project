f02e09a96e3ed50a957617100150ea249708045e
acyadigar <alpercanyadigar@gmail.com>
2021-12-09 00:27:43 +0300

    Add user components

---
+++ src/components/UserListItem.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: ['user'],
+}
+</script>
+
+<template>
+  <li class="user">
+    <span>
+      <img :src="user.avatar">
+      {{user.name}}
+    </span>
+    <span>{{user.createdAt.split('T')[0].replaceAll('-', '/')}}</span>
+    <span>{{user.email}}</span>
+    <span>{{user.phone}}</span>
+  </li>
+</template>
+
+<style>
+
+</style>
